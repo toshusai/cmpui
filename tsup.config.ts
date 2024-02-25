d075d967029884ee6b8bc9fc94f4d9acdee26d6f
toshusai <toshusai.net@gmail.com>
2024-02-25 20:35:23 +0900

    fix: build settings

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  tsconfig: "tsconfig.build.json",
   entryPoints: ["src/index.ts"],
   format: "esm",
   target: "esnext",
