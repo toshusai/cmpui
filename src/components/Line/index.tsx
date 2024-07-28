7c78971066a72e16b61b475a84fb747d8f8b61ca
toshusai <35446607+toshusai@users.noreply.github.com>
2024-07-28 11:16:05 +0900

    feat: RectGizmoの動作を更新

--- src/components/Line/index.tsx
+++ src/components/Line/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 
 export type LineProps = {
   strokeWidth: number;
@@ -6,32 +6,41 @@ export type LineProps = {
   x2: number;
   y1: number;
   y2: number;
+  adjust?: boolean;
+  pointerEvents?: CSSProperties["pointerEvents"];
 } & React.SVGProps<SVGLineElement>;
 
-export function Line(props: LineProps) {
+export function Line({
+  adjust = true,
+  pointerEvents = "none",
+  style,
+  ...props
+}: LineProps) {
   const width = Math.abs(props.x2 - props.x1);
   const height = Math.abs(props.y2 - props.y1);
   const padding = props.strokeWidth;
+  const adjustPadding = adjust === true ? props.strokeWidth / 2 : 0;
   return (
     <svg
       width={width + padding}
       height={height + padding}
       viewBox={`0 0 ${width + padding} ${height + padding}`}
-      className={props.className}
       style={{
         position: "absolute",
         overflow: "visible",
-        pointerEvents: "none",
-        left: Math.min(props.x1, props.x2) - padding / 2,
-        top: Math.min(props.y1, props.y2) - padding / 2,
+        pointerEvents,
+        left: Math.min(props.x1, props.x2) - adjustPadding,
+        top: Math.min(props.y1, props.y2) - adjustPadding,
+        ...style,
       }}
     >
       <line
+        className={props.className}
         {...props}
-        x1={props.x1 - Math.min(props.x1, props.x2) + padding / 2}
-        y1={props.y1 - Math.min(props.y1, props.y2) + padding / 2}
-        x2={props.x2 - Math.min(props.x1, props.x2) + padding / 2}
-        y2={props.y2 - Math.min(props.y1, props.y2) + padding / 2}
+        x1={props.x1 - Math.min(props.x1, props.x2) + adjustPadding}
+        y1={props.y1 - Math.min(props.y1, props.y2) + adjustPadding}
+        x2={props.x2 - Math.min(props.x1, props.x2) + adjustPadding}
+        y2={props.y2 - Math.min(props.y1, props.y2) + adjustPadding}
       />
     </svg>
   );
---
+++ src/components/RectGizmo/corners.ts
@@ -0,0 +1,6 @@
+export const corners = [
+  [0, 0],
+  [0, 1],
+  [1, 1],
+  [1, 0],
+] as const;
