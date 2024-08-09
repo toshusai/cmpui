22cc8ed2bb4053c6fa9b578e5dd7cbe2c65db0c0
toshusai <35446607+toshusai@users.noreply.github.com>
2024-08-09 12:50:18 +0900

    fix: 不要なファイルの更新を戻す

--- src/components/Line/index.tsx
+++ src/components/Line/index.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from "react";
+import React from "react";
 
 export type LineProps = {
   strokeWidth: number;
@@ -6,41 +6,32 @@ export type LineProps = {
   x2: number;
   y1: number;
   y2: number;
-  adjust?: boolean;
-  pointerEvents?: CSSProperties["pointerEvents"];
 } & React.SVGProps<SVGLineElement>;
 
-export function Line({
-  adjust = true,
-  pointerEvents = "none",
-  style,
-  ...props
-}: LineProps) {
+export function Line(props: LineProps) {
   const width = Math.abs(props.x2 - props.x1);
   const height = Math.abs(props.y2 - props.y1);
   const padding = props.strokeWidth;
-  const adjustPadding = adjust === true ? props.strokeWidth / 2 : 0;
   return (
     <svg
       width={width + padding}
       height={height + padding}
       viewBox={`0 0 ${width + padding} ${height + padding}`}
+      className={props.className}
       style={{
         position: "absolute",
         overflow: "visible",
-        pointerEvents,
-        left: Math.min(props.x1, props.x2) - adjustPadding,
-        top: Math.min(props.y1, props.y2) - adjustPadding,
-        ...style,
+        pointerEvents: "none",
+        left: Math.min(props.x1, props.x2) - padding / 2,
+        top: Math.min(props.y1, props.y2) - padding / 2,
       }}
     >
       <line
-        className={props.className}
         {...props}
-        x1={props.x1 - Math.min(props.x1, props.x2) + adjustPadding}
-        y1={props.y1 - Math.min(props.y1, props.y2) + adjustPadding}
-        x2={props.x2 - Math.min(props.x1, props.x2) + adjustPadding}
-        y2={props.y2 - Math.min(props.y1, props.y2) + adjustPadding}
+        x1={props.x1 - Math.min(props.x1, props.x2) + padding / 2}
+        y1={props.y1 - Math.min(props.y1, props.y2) + padding / 2}
+        x2={props.x2 - Math.min(props.x1, props.x2) + padding / 2}
+        y2={props.y2 - Math.min(props.y1, props.y2) + padding / 2}
       />
     </svg>
   );
