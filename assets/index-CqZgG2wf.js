7b65c01f17a417324f99fb6592bd57e7e48d5dd3
toshusai <toshusai@users.noreply.github.com>
2024-08-28 14:49:38 +0000

    deploy: 038bf2ad20984a04aff0a0184045e623604e812b

---
+++ assets/index-CqZgG2wf.js
@@ -0,0 +1,17 @@
+import{j as u}from"./jsx-runtime-Cw0GR0a5.js";import{r as y}from"./index-CTjT7uj6.js";import{c as g}from"./createDragHandler-DDNYS428.js";import{C as b}from"./index-VWCGd7j2.js";import{L as x}from"./index-Et-kLNzN.js";import{P as E}from"./index-C3vRDm1t.js";function T({prev:l,cubic:r,onChange:e}){const{x1:m,y1:o,x2:a,y2:s,x:v,y:k}=r,{x:q,y:_}=l,d={type:"move",x:l.x,y:l.y},c=y.useCallback((n,t,P)=>g({onDown:i=>({x:i.clientX,y:i.clientY}),onMove:(i,p)=>{if(!p)return;const D=i.clientX-p.x,N=i.clientY-p.y,M=n+D,W=t+N;P(M,W)}}),[]),f=y.useMemo(()=>c(m,o,(n,t)=>{e==null||e({...r,x1:n,y1:t})}),[r,m,o,e,c]),j=y.useMemo(()=>c(a,s,(n,t)=>{e==null||e({...r,x2:n,y2:t})}),[r,a,s,e,c]),w=y.useMemo(()=>[d,r],[d,r]);return u.jsxs(u.Fragment,{children:[u.jsx(E,{commands:w,stroke:"var(--cmpui-primary-color)",style:{pointerEvents:"none"}}),u.jsx(x,{className:"cmpui_cubic-controls__line",strokeWidth:1,x1:q,y1:_,x2:m,y2:o}),u.jsx(x,{className:"cmpui_cubic-controls__line",strokeWidth:1,x1:v,y1:k,x2:a,y2:s}),u.jsx(b,{className:"cmpui_cubic-controls__circle",x:m,y:o,radius:6,onPointerDown:f,fill:"var(--cmpui-primary-color)",strokeColor:"var(--cmpui-border-color)",strokeWidth:1}),u.jsx(b,{className:"cmpui_cubic-controls__circle",x:a,y:s,radius:6,onPointerDown:j,fill:"var(--cmpui-primary-color)",strokeColor:"var(--cmpui-border-color)",strokeWidth:1})]})}T.__docgenInfo={description:"",methods:[],displayName:"CubicControls",props:{prev:{required:!0,tsType:{name:"signature",type:"object",raw:"{ x: number; y: number }",signature:{properties:[{key:"x",value:{name:"number",required:!0}},{key:"y",value:{name:"number",required:!0}}]}},description:""},cubic:{required:!0,tsType:{name:"signature",type:"object",raw:`{
+  type: "cubic";
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  x: number;
+  y: number;
+}`,signature:{properties:[{key:"type",value:{name:"literal",value:'"cubic"',required:!0}},{key:"x1",value:{name:"number",required:!0}},{key:"y1",value:{name:"number",required:!0}},{key:"x2",value:{name:"number",required:!0}},{key:"y2",value:{name:"number",required:!0}},{key:"x",value:{name:"number",required:!0}},{key:"y",value:{name:"number",required:!0}}]}},description:""},onChange:{required:!1,tsType:{name:"signature",type:"function",raw:"(cubic: CubicCommand) => void",signature:{arguments:[{type:{name:"signature",type:"object",raw:`{
+  type: "cubic";
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  x: number;
+  y: number;
+}`,signature:{properties:[{key:"type",value:{name:"literal",value:'"cubic"',required:!0}},{key:"x1",value:{name:"number",required:!0}},{key:"y1",value:{name:"number",required:!0}},{key:"x2",value:{name:"number",required:!0}},{key:"y2",value:{name:"number",required:!0}},{key:"x",value:{name:"number",required:!0}},{key:"y",value:{name:"number",required:!0}}]}},name:"cubic"}],return:{name:"void"}}},description:""}}};export{T as C};
---
+++ assets/index.stories-Dyl0PhgB.js
@@ -0,0 +1,43 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{r}from"./index-CTjT7uj6.js";import{j as s,k as p}from"./index-CTZGfwXl.js";import"./index-DjOCQ1f2.js";import"./createDragHandler-DDNYS428.js";import"./index-VWCGd7j2.js";import"./classNames-CN4lTu6a.js";import"./px-DWM1exbb.js";import"./index-Et-kLNzN.js";import"./index-CVWKsAoW.js";import"./alpha-checker-board-BYXq6jsz.js";import"./index-CI0rqRLE.js";import"./index-Bk7gfChQ.js";import"./clamp-co6UzHBn.js";import"./index-B1Pf1uP_.js";import"./index-D9nq7hlE.js";import"./index-BVhRoD2p.js";import"./index-DCmEyuZs.js";import"./index-D3iKhckW.js";import"./index-CYM-y3Gt.js";import"./index-kTCvGdNn.js";import"./index-BpKo_AiQ.js";import"./index-sQDGARt6.js";import"./index-CqZgG2wf.js";import"./index-C3vRDm1t.js";import"./DropdownMenuItem-DoBa3JPr.js";import"./index-D9aJJu6P.js";import"./index-DpXXaser.js";import"./IconButtonGroup-CNqVmcEZ.js";import"./index-D38S78TV.js";import"./index-BPVpoRAo.js";import"./mergeRefs-D1znqPHH.js";import"./index-BhDvpG6f.js";import"./index-FzlHB9oK.js";import"./index-CL60H3As.js";import"./index-BoQn9C7d.js";import"./index-CEZRDE7O.js";import"./index-DD1un7QO.js";const ve={title:"Form/Select",component:s},d=[{label:"Option 1",value:"1"},{label:"Option 2",value:"2"},{label:"Option 3",value:"3"},{label:"Option 4",value:"4"}],i={render:function(){const[a,l]=r.useState("1");return o.jsx(s,{label:"Label",value:a,onChange:l,children:d.map(t=>o.jsx(p,{value:t.value,children:t.label},t.value))})}},u={render:function(){const[a,l]=r.useState("1");return o.jsx(s,{label:"Label",value:a,onChange:l,disabled:!0,children:d.map(t=>o.jsx(p,{value:t.value,children:t.label},t.value))})}},m={render:function(){const[a,l]=r.useState("1");return o.jsx(s,{label:"Label",value:a,onChange:l,children:d.map(t=>o.jsx(p,{value:t.value,disabled:t.value==="2",children:t.label},t.value))})}},F=["Roboto","Sixtyfour","Wavefont","Gabarito","Agbalumo","Honk","Slackside One","Smooch","Chokokutai","Monomaniac One","Palette Mosaic"].sort(),c={render:function(){const a=F.map(n=>({label:n,value:n,family:n,url:`https://fonts.googleapis.com/css2?family=${n}&display=swap`})),[l,t]=r.useState("Roboto");return o.jsx(s,{label:"font",value:l,onChange:t,children:a.map(n=>o.jsx(L,{family:n.family,url:n.url,value:n.value,children:n.label},n.value))})}},v=new Map;function I(e){if(v.has(e))return;const a=document.createElement("link");a.href=e,a.rel="stylesheet",document.head.appendChild(a),v.set(e,e)}function L(e){return r.useEffect(()=>{I(e.url)},[e.url]),o.jsx(p,{value:e.value,children:o.jsx("div",{style:{fontFamily:e.family},children:e.children})})}var f,b,S;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: function Render() {
+    const [value, setValue] = useState("1");
+    return <Select label="Label" value={value} onChange={setValue}>
+        {options.map(option => <SelectItem value={option.value} key={option.value}>
+            {option.label}
+          </SelectItem>)}
+      </Select>;
+  }
+}`,...(S=(b=i.parameters)==null?void 0:b.docs)==null?void 0:S.source}}};var h,y,g;u.parameters={...u.parameters,docs:{...(h=u.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: function Render() {
+    const [value, setValue] = useState("1");
+    return <Select label="Label" value={value} onChange={setValue} disabled>
+        {options.map(option => <SelectItem value={option.value} key={option.value}>
+            {option.label}
+          </SelectItem>)}
+      </Select>;
+  }
+}`,...(g=(y=u.parameters)==null?void 0:y.docs)==null?void 0:g.source}}};var x,R,j;m.parameters={...m.parameters,docs:{...(x=m.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: function Render() {
+    const [value, setValue] = useState("1");
+    return <Select label="Label" value={value} onChange={setValue}>
+        {options.map(option => <SelectItem value={option.value} key={option.value} disabled={option.value === "2"}>
+            {option.label}
+          </SelectItem>)}
+      </Select>;
+  }
+}`,...(j=(R=m.parameters)==null?void 0:R.docs)==null?void 0:j.source}}};var V,k,C;c.parameters={...c.parameters,docs:{...(V=c.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  render: function Render() {
+    const fonts = fontList.map(font => ({
+      label: font,
+      value: font,
+      family: font,
+      url: \`https://fonts.googleapis.com/css2?family=\${font}&display=swap\`
+    }));
+    const [value, setValue] = useState("Roboto");
+    return <Select label="font" value={value} onChange={setValue}>
+        {fonts.map(font => <FontRender family={font.family} url={font.url} value={font.value} key={font.value}>
+            {font.label}
+          </FontRender>)}
+      </Select>;
+  }
+}`,...(C=(k=c.parameters)==null?void 0:k.docs)==null?void 0:C.source}}};const fe=["Basic","Disabled","DisabledItem","Fonts"];export{i as Basic,u as Disabled,m as DisabledItem,c as Fonts,fe as __namedExportsOrder,ve as default};
